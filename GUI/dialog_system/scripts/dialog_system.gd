f427db31cdc62846b0593a923d479f52a805d065
Ice4113 <tteenboyx@gmail.com>
2024-12-22 10:59:43 +0700

    update
    update

--- GUI/dialog_system/scripts/dialog_system.gd
+++ GUI/dialog_system/scripts/dialog_system.gd
@@ -7,6 +7,7 @@ signal letter_added(letter :String)
 
 var is_active : bool = false
 var text_in_progess : bool = false
+var waiting_for_choices : bool =false
 
 var dialog_items : Array[DialogItem]
 var dialog_item_index : int = 0
@@ -23,6 +24,7 @@ var text_length : int = 0
 @onready var dialog_progress_indicator_label: Label = $DialogUI/DialogProgressIndicator/Label
 @onready var timer: Timer = $DialogUI/Timer
 @onready var audio_stream_player: AudioStreamPlayer = $DialogUI/AudioStreamPlayer
+@onready var choice_options: VBoxContainer = $DialogUI/VBoxContainer
 
 
 
@@ -52,6 +54,8 @@ func _unhandled_input(event: InputEvent) -> void:
 			text_in_progess = false
 			show_dialog_button_indicator(true)
 			return 
+		elif waiting_for_choices == true:
+			return 
 		dialog_item_index += 1
 		if dialog_item_index < dialog_items.size():
 			start_dialog()
@@ -73,6 +77,7 @@ func show_dialog(_items : Array[DialogItem]) -> void:
 
 func hide_dialog() -> void:
 	is_active = false
+	choice_options.visible = false
 	dialog_ui.visible = false
 	dialog_ui.process_mode = Node.PROCESS_MODE_DISABLED
 	get_tree().paused = false 
@@ -82,16 +87,57 @@ func hide_dialog() -> void:
 
 func start_dialog() -> void: 
 	#print(dialog_item_index)
+	waiting_for_choices = false
 	show_dialog_button_indicator(false)
 	var _d : DialogItem = dialog_items[dialog_item_index]
-	set_dialog_data(_d)
 	
+	if _d is DialogText:
+		set_dialog_text(_d as DialogText)
+	elif _d is DialogChoice:
+		set_dialog_choice(_d as DialogChoice)
+	
+	pass
+
+
+func set_dialog_text(_d :DialogItem) -> void:
+	content.text = _d.text
+	choice_options.visible = false
+	name_label.text = _d.npc_info.npc_name
+	potrait_sprite.texture = _d.npc_info.potrait
+	potrait_sprite.audio_pitch_base = _d.npc_info.dialog_audio_pitch
 	content.visible_characters = 0
 	text_length = content.get_total_character_count()
 	plain_text = content.get_parsed_text()
 	text_in_progess = true
 	start_timer()
 	pass
+
+func set_dialog_choice(_d :DialogChoice) -> void:
+	choice_options.visible = true
+	waiting_for_choices = true
+	for c in choice_options.get_children():
+		c.queue_free()
+	
+	
+	for i in _d.dialog_branches.size():
+		var _new_choice : Button = Button.new()
+		_new_choice.text = _d.dialog_branches[i].text
+		_new_choice.alignment = HORIZONTAL_ALIGNMENT_LEFT
+		_new_choice.pressed.connect(_dialog_choice_selected.bind(_d.dialog_branches[i]))
+		choice_options.add_child(_new_choice)
+	
+	if Engine.is_editor_hint():
+		return
+	 
+	await get_tree().process_frame
+	choice_options.get_child(0).grab_focus()
+	pass
+
+func _dialog_choice_selected(_d :DialogBranch) -> void:
+	choice_options.visible = false 
+	show_dialog(_d.dialog_items)
+	pass
+
 
 func _on_timer_timeout() -> void:
 	content.visible_characters += 1
@@ -115,14 +161,6 @@ func start_timer() -> void:
 
 
 
-func set_dialog_data(_d :DialogItem) -> void:
-	if _d is DialogText:
-		content.text = _d.text
-	name_label.text = _d.npc_info.npc_name
-	potrait_sprite.texture = _d.npc_info.potrait
-	print("audio_pitch:", _d.npc_info.dialog_audio_pitch) 
-	potrait_sprite.audio_pitch_base = _d.npc_info.dialog_audio_pitch
-	pass
 
 func show_dialog_button_indicator(_is_visble : bool) -> void:
 	dialog_progress_indicator.visible = _is_visble
